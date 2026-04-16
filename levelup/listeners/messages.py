5e27c2c46cb502329d8858dad08cbdbbf30a1243
MW623 <135554320+MW623@users.noreply.github.com>
2026-04-16 18:14:11 +0100

    Update messages.py

--- levelup/listeners/messages.py
+++ levelup/listeners/messages.py
@@ -119,9 +119,8 @@ class MessageListener(MixinMeta):
             # If we're specifically ignoring a user we don't want to see them anywhere
             return
 
-        BLOCKED_CHANNEL_ID = 734241866933076048
-        if message.channel.id == BLOCKED_CHANNEL_ID:
-            # Specifically blocks #bot-commands in RDB7
+        # Ignore messages starting with "."
+        if message.content.startswith("."):
             return
 
         profile = conf.get_profile(user_id).add_message()
